a115c028edd3691120dca8c11b09e696dc45f6e9
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-17 09:21:47 -0600

    re-ordered everything

--- snapChallenge.php
+++ snapChallenge.php
@@ -6,25 +6,20 @@
  * Time: 8:07 AM
  */
 
+// variables
+$foo = "Here's a string";
+$bar = " that I'm concatenating";
+$anotherTestArray = array(0, 'apple', array("here's", "another", "array"), 'banana');
+
 // number function
 function average($a, $b) {
 	return ($a + $b) / 2;
 }
 
-echo average(5, 4);
-echo "<br><br>";
-
 // string function
 function joinString($x, $y) {
 	return ($x = $x . $y);
 }
-
-$foo = "Here's a string";
-$bar = " that I'm concatenating";
-
-echo joinString($foo, $bar);
-echo "<br><br>";
-
 
 //array functions
 $testArray = array();
@@ -35,13 +30,14 @@ function fillInArray() {
 		echo "<br><br>";
 }
 
-fillInArray();
-
-$anotherTestArray = array(0, 'apple', array("here's", "another", "array"), 'banana');
-
 function anotherArrayFunction($test) {
 	print_r(array_values($test));
 }
 
-
+// call everything
+echo average(5, 4);
+echo "<br><br>";
+echo joinString($foo, $bar);
+echo "<br><br>";
+fillInArray();
 anotherArrayFunction($anotherTestArray);
