001872d55d79de1932077412d05a77d45c1ac3de
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-17 08:42:55 -0600

    added another array function

--- snapChallenge.php
+++ snapChallenge.php
@@ -10,7 +10,7 @@ function average($a, $b) {
 }
 
 echo average(5, 4);
-echo "<br>";
+echo "<br><br>";
 
 function joinString($x, $y) {
 	return ($x = $x . $y);
@@ -20,12 +20,22 @@ $foo = "Here's a string";
 $bar = " that I'm concatenating";
 
 echo joinString($foo, $bar);
-echo "<br>";
+echo "<br><br>";
 
 
 function fillInArray() {
 		$testArray = array_fill(0, 10, 'Hi George');
 		print_r($testArray);
+		echo "<br><br>";
 }
 
 fillInArray();
+
+$anotherTestArray = array(0, 'apple', array(0, 1, 2), 'banana');
+
+function anotherArrayFunction($test) {
+	print_r(array_values($test));
+}
+
+
+anotherArrayFunction($anotherTestArray);
